c9253806645d8b6657836cb24f4ddb0a032ff6a9
DimaOsinnii <ozerland@gmail.com>
2021-01-30 13:43:06 +0200

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./styles/index.less";
+
+export default () => (
+  <>
+    <h1>GitHub Search</h1>
+  </>
+);
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+module.exports = () => ({
+  entry: {
+    index: "./src/index.js",
+  },
+  output: {
+    filename: "js/[name].bundle.js",
+    path: __dirname + "/dist",
+  },
+  optimization: {
+    splitChunks: { chunks: "all" },
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+        test: /\.less|.css$/,
+        use: ["style-loader", "css-loader", "less-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new ESLintPlugin({
+      extensions: ["js", "jsx"],
+    }),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new HtmlWebpackPlugin({
+      title: "Development",
+      template: "src/index.html",
+      inject: "body",
+    }),
+  ],
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+    writeToDisk: true,
+    open: true,
+  },
+});
